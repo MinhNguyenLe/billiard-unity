0c1001a515e68243cd66bba04d422097b0fc945b
toan5ks1 <75209339+toan5ks1@users.noreply.github.com>
2021-07-18 21:08:40 +0700

    update

--- Assets/Scripts/HostBallCotroler.cs
+++ Assets/Scripts/HostBallCotroler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class demo : MonoBehaviour
+public class HostBallCotroler : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
